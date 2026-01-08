1c50894fa2d128d29d8e69cf20ff2153339f2a18
Navnath Korde <navnathk545@gmail.com>
2026-01-08 17:17:13 +0530

    Largest Element IN Array

---
+++ ProblemSolving/LargestElementInArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    internal class LargestElementInArray
+    {
+        public static int FindLargestInArray(int[] arr, int n)
+        {
+            int largest = arr[0];
+
+            for(int i = 1; i < n; i++)
+            {
+                if (arr[i] > largest)
+                {
+                    largest = arr[i];
+                }
+            }
+            return largest;
+        }
+        public static void Main(string[] args)
+        {
+            int[] arr = { 3, 5, 7, 2, 8, -1, 4 };
+            int n = arr.Length;
+            int temp
+                = FindLargestInArray(arr, n);
+            Console.WriteLine(temp);
+        }
+    }
+}
